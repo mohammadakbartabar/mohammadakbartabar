7f8cb9b4c3a9f4ea22dd537794fb0e2b44f439ba
mohammadakbartabar <84190503+mohammadakbartabar@users.noreply.github.com>
2021-05-14 18:51:02 +0430

    Add files via upload

---
+++ sal kabise.py
@@ -0,0 +1,13 @@
+
+print("salam ")
+name= input("esm khod ra benevisid"":")
+print("khosh amadi",name)
+sal=int(input("sal madnazar khod ra vared konid"":"))
+if (sal%33 == 1) or (sal%33 == 5)\
+      or(sal%33 == 9) or (sal%33 == 13)\
+            or (sal%33 == 17) or (sal%33 == 22)\
+                 or (sal%33 == 26) or (sal%33 == 30) :
+                 print("sal madnazar kabise ast")
+else:
+      print("sal madnazar kabise nist")
+
